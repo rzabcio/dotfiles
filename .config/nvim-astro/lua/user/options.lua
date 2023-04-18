e3170104bc2a8a3d49e7e7d7e46e35727616d1c7
Jakub Głazik <rzabcio@gmail.com>
2023-04-18 06:51:33 +0200

    feat: breakindent settings

--- .config/nvim-astro/lua/user/options.lua
+++ .config/nvim-astro/lua/user/options.lua
@@ -2,21 +2,26 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
     mouse = "",
+    number = true, -- sets vim.opt.number
+    relativenumber = true, -- sets vim.opt.relativenumber
+    showbreak='↳ ', -- show break line
+    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    spell = false, -- sets vim.opt.spell
+    wrap = false, -- sets vim.opt.wrap
   },
   g = {
-    mapleader = " ", -- sets vim.g.mapleader
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
     autopairs_enabled = true, -- enable autopairs at start
+    cmp_enabled = true, -- enable completion at start
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    mapleader = " ", -- sets vim.g.mapleader
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+  },
+  wo = {
+    breakindent=true,-- Envable indentation at break line
+    breakindentopt='shift:4,min:40,sbr',
   },
 }
 -- If you need more control, you can use the function()...end notation
