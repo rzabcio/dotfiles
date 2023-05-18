c75429fb149bcc3e5eb826758f9df4c8b9765bc4
Jakub Głazik <rzabcio@gmail.com>
2023-05-18 14:42:24 +0200

    feat(nvim): added invisible chars (list/listchars), disable auto-pairing paretheses and quotas

--- .config/nvim-astro/lua/user/options.lua
+++ .config/nvim-astro/lua/user/options.lua
@@ -2,6 +2,8 @@
 return {
   opt = {
     -- set to true or false etc.
+    list = true,
+    listchars = 'tab:  ‣,trail:␣,extends:→,precedes:←,nbsp:␣',
     mouse = "",
     number = true, -- sets vim.opt.number
     relativenumber = true, -- sets vim.opt.relativenumber
@@ -12,7 +14,7 @@ return {
   },
   g = {
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    autopairs_enabled = true, -- enable autopairs at start
+    autopairs_enabled = false, -- enable autopairs at start
     cmp_enabled = true, -- enable completion at start
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
@@ -20,7 +22,7 @@ return {
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
   wo = {
-    breakindent=true,-- Envable indentation at break line
+    breakindent=true,-- Enable indentation at break line
     breakindentopt='shift:4,min:40,sbr',
   },
 }
