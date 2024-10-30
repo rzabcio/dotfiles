111e19efe7814ebb70748be66ca3b27deb72adde
Jakub Głazik <rzabcio@gmail.com>
2024-10-30 10:51:29 +0100

    fix(nvim): testing new themes (but everforest still rules...)

--- .config/nvim-astro/lua/plugins/astroui.lua
+++ .config/nvim-astro/lua/plugins/astroui.lua
@@ -11,9 +11,9 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "everforest",
+    -- colorscheme = "everforest",
     -- colorscheme = "astromars",
-    -- colorscheme = "eldritch",
+    colorscheme = "eldritch",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
