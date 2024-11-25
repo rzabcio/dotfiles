b53c06305dfb9b3a721021fc575bc429161d9d1f
Jakub Glazik <rzabcio@gmail.com>
2024-11-25 20:39:57 +0100

    feat(nvim): auto-formatting md on save

--- .config/nvim-astro/lua/plugins/astrolsp.lua
+++ .config/nvim-astro/lua/plugins/astrolsp.lua
@@ -23,6 +23,7 @@ return {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           "go",
+          "markdown",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
