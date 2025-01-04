aa51cdbb21c7f4a54f14d135e89cfdc6635e0890
Jakub Głazik <rzabcio@gmail.com>
2025-01-04 21:31:40 +0100

    feat(nvim): let's stick with neofusion, somehow this blue/red mix grew up on me

--- .config/nvim-astro/lua/plugins/astroui.lua
+++ .config/nvim-astro/lua/plugins/astroui.lua
@@ -14,8 +14,8 @@ return {
     -- colorscheme = "everforest",
     -- colorscheme = "astromars",
     -- colorscheme = "eldritch",
-    -- colorscheme = "neofusion",
-    colorscheme = "flow",
+    colorscheme = "neofusion",
+    -- colorscheme = "flow",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
