4191fc7c50d6fc7fe6c9a6be20cef06bd9a65210
Jakub Głazik <rzabcio@gmail.com>
2025-04-09 09:38:12 +0200

    feat(nvim): added parrot with perplexity for test purposes

--- .config/nvim-astro/lua/plugins/user_editing.lua
+++ .config/nvim-astro/lua/plugins/user_editing.lua
@@ -31,4 +31,19 @@ return {
     event = "VeryLazy",
     config = function() require("nvim-surround").setup {} end,
   },
+  {
+    "frankroeder/parrot.nvim",
+    dependencies = { 'ibhagwan/fzf-lua', 'nvim-lua/plenary.nvim' },
+    -- optionally include "folke/noice.nvim" or "rcarriga/nvim-notify" for beautiful notifications
+    config = function()
+      require("parrot").setup {
+        -- Providers must be explicitly added to make them available.
+        providers = {
+          pplx = {
+            api_key = os.getenv "PERPLEXITY_API_KEY",
+          },
+        },
+      }
+    end,
+  }
 }
